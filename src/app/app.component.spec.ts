ce88237b652452c1405f979d00b37d9023adc936
Tamara Cook <10754072+tamaracha@users.noreply.github.com>
2020-06-10 16:22:47 +0200

    Fix warnings in tests about missing modules

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,8 @@
 /* eslint-env jasmine */
 import { TestBed, async } from '@angular/core/testing'
 import { FormsModule } from '@angular/forms'
+import { MatToolbarModule } from '@angular/material/toolbar'
+import { MatInputModule } from '@angular/material/input'
 import { MatGridListModule } from '@angular/material/grid-list'
 import { AppComponent } from './app.component'
 import { KeysFormComponent } from './keys-form/keys-form.component'
@@ -8,7 +10,7 @@ import { KeysFormComponent } from './keys-form/keys-form.component'
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [FormsModule, MatGridListModule],
+      imports: [FormsModule, MatGridListModule, MatInputModule, MatToolbarModule],
       declarations: [
         AppComponent,
         KeysFormComponent
