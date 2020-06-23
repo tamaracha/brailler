59fb1984e8437dd3ca24d65943aef203f2e9d2cd
Tamara Cook <10754072+tamaracha@users.noreply.github.com>
2020-06-23 10:43:09 +0200

    Style keys-form in css and remove material dependencies

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,22 +1,13 @@
 import { NgModule } from '@angular/core'
-import { FlexLayoutModule } from '@angular/flex-layout'
-import { MatGridListModule } from '@angular/material/grid-list'
-import { MatToolbarModule } from '@angular/material/toolbar'
 import { MatInputModule } from '@angular/material/input'
 
 @NgModule({
   declarations: [],
   imports: [
-    FlexLayoutModule,
-    MatGridListModule,
-    MatInputModule,
-    MatToolbarModule
+    MatInputModule
   ],
   exports: [
-    FlexLayoutModule,
-    MatGridListModule,
-    MatInputModule,
-    MatToolbarModule
+    MatInputModule
   ]
 })
 export class MaterialModule { }
